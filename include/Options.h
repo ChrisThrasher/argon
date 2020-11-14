2b42a290a7b5209f95713a252f5575ca77102795
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-13 23:25:41 -0700

    Added support for parsing ints

--- include/Options.h
+++ include/Options.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <optional>
 #include <string>
 #include <vector>
 
@@ -11,6 +12,7 @@ class Options
 public:
     Options(const int, const char* const[]);
     void ExitItem(const std::vector<std::string>&, const std::string&) const;
+    std::optional<int> Int(const std::vector<std::string>&) const;
 };
 
 Options::Options(const int argc, const char* const argv[])
@@ -24,4 +26,13 @@ void Options::ExitItem(const std::vector<std::string>& flags, const std::string&
         for (const auto& flag : flags)
             if (arg == flag)
                 std::cout << text << '\n';
+}
+
+std::optional<int> Options::Int(const std::vector<std::string>& flags) const
+{
+    for (size_t i = 0; i < args.size(); ++i)
+        for (const auto& flag : flags)
+            if (args[i] == flag)
+                return std::stoi(args.at(i + 1));
+    return std::nullopt;
 }
