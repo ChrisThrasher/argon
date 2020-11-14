2be535f5415e17808d40b5b40e145bd3342ff3dc
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-14 15:38:45 -0700

    Improved const-correctness of tests

--- test/UnitTests.cpp
+++ test/UnitTests.cpp
@@ -38,7 +38,7 @@ TEST(ExitItem, Checkout)
 {
     constexpr int argc = 3;
     constexpr const char* argv[argc] = {"options", "--flag", "-v"};
-    Options options(argc, argv);
+    const Options options(argc, argv);
 
     EXPECT_EXIT(options.ExitItem({"--flag"}, ""), testing::ExitedWithCode(0), "");
     options.ExitItem({"-v"}, "this won't be detected");
@@ -48,7 +48,8 @@ TEST(At, Checkout)
 {
     constexpr int argc = 3;
     constexpr const char* argv[argc] = {"program_name", "first_arg", "second_arg"};
-    Options options(argc, argv);
+    const Options options(argc, argv);
+
     EXPECT_EQ("program_name", options.At(0, ""));
     EXPECT_EQ("first_arg", options.At(1, ""));
     EXPECT_EQ("second_arg", options.At(2, ""));
