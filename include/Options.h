afa198dec21255c920cfe4278b471b73cffe6d14
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-13 23:38:54 -0700

    Added support for accessing positional arguments

--- include/Options.h
+++ include/Options.h
@@ -11,12 +11,15 @@ class Options
 public:
     Options(const int, const char* const[]);
     void ExitItem(const std::vector<std::string>&, const std::string&) const;
+    auto At(const int) const -> std::string;
 };
 
 Options::Options(const int argc, const char* const argv[])
-    : args(std::vector<std::string>(argv + 1, argv + argc))
+    : args(std::vector<std::string>(argv, argv + argc))
 {
 }
+
+auto Options::At(const int pos) const -> std::string { return args.at(pos); }
 
 void Options::ExitItem(const std::vector<std::string>& flags, const std::string& text) const
 {
--- src/Options.cpp
+++ src/Options.cpp
@@ -5,4 +5,5 @@ int main(int argc, char* argv[])
     Options options(argc, argv);
     options.ExitItem({"-h", "--help"}, "this is my help text");
     options.ExitItem({"-v", "--version"}, "v0.0.0");
+    std::cout << options.At(1) << '\n';
 }
