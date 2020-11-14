f3ab6504435efd95591cb53de1227f021fd61605
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-13 23:50:43 -0700

    Added unit test executable

---
+++ test/UnitTests.cpp
@@ -0,0 +1,20 @@
+#include <Options.h>
+
+#include <gtest/gtest.h>
+
+TEST(Options, At)
+{
+    constexpr int argc = 3;
+    constexpr const char* argv[argc] = {"program_name", "first_arg", "second_arg"};
+    Options options(argc, argv);
+    EXPECT_EQ("program_name", options.At(0));
+    EXPECT_EQ("first_arg", options.At(1));
+    EXPECT_EQ("second_arg", options.At(2));
+    EXPECT_THROW(options.At(3), std::out_of_range);
+}
+
+int main(int argc, char* argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
