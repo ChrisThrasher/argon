82007413314112bfa6b657146ca7f361c97456e6
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-21 12:25:43 -0700

    Improved const correctness

--- include/Options/Types.h
+++ include/Options/Types.h
@@ -9,23 +9,27 @@ namespace opts
 
 struct ExitOption
 {
-    std::vector<std::string> flags;
+    const std::vector<std::string> flags;
     const std::string description;
     const std::function<std::string()> output;
 
     ExitOption(const std::vector<std::string>& a_flags,
                const std::string& a_description,
                const std::function<std::string()>& a_output)
-        : description(a_description)
+        : flags([a_flags]() {
+            std::vector<std::string> tmp;
+            for (const auto& flag : a_flags)
+            {
+                if (flag.size() == 1)
+                    tmp.push_back('-' + flag);
+                else
+                    tmp.push_back("--" + flag);
+            }
+            return tmp;
+        }())
+        , description(a_description)
         , output(a_output)
     {
-        for (const auto& flag : a_flags)
-        {
-            if (flag.size() == 1)
-                flags.push_back('-' + flag);
-            else
-                flags.push_back("--" + flag);
-        }
     }
 };
 
