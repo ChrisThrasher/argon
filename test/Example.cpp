898893ccc240c022da038202e3d28ca76365aec1
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-22 13:32:11 -0700

    Added tests for multiple boolean flags

--- test/Example.cpp
+++ test/Example.cpp
@@ -6,12 +6,16 @@ constexpr auto help = R"(Usage
 int main(int argc, char* argv[])
 {
     bool debug = false;
+    bool verbose = false;
 
     opts::Parser parser(argc, argv, help);
-    parser.Add(opts::ExitOption("version,v", "Print program version", "v0.0.0"));
-    parser.Add(opts::BoolOption("debug", "Debug output", debug));
+    parser.Add(opts::ExitOption("version", "Print program version", "v0.0.0"));
+    parser.Add(opts::BoolOption("d,debug", "Debug output", debug));
+    parser.Add(opts::BoolOption("v,verbose", "Verbose output", verbose));
     parser.Parse();
 
     if (debug)
         std::cout << "Found debug flag\n";
+    if (verbose)
+        std::cout << "Found verbose flag\n";
 }
