1f344cb1e6a2cedfa1cce553cf53311009596f26
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-11-27 00:02:47 -0600

    Tweaked example code

--- test/Example.cpp
+++ test/Example.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
     int count = 0;
     float temp = 0.0f;
     double speed = 0.0;
-    std::string filename = "/file/not/specificed";
+    std::string filename = "";
 
     opts::Parser parser(argc, argv, help);
     parser.Add("version", "Print program version", opts::Print("v0.0.0"));
