80562140c62857107e707b0640d2a615d7528a34
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-12-12 18:13:27 -0700

    Rearranged some code

--- include/Options/Parser.h
+++ include/Options/Parser.h
@@ -7,37 +7,6 @@
 namespace opts
 {
 
-auto Usage(const std::string& output) -> std::string { return output; }
-
-auto Print(const std::string& output) -> std::function<void()>
-{
-    return [output]() {
-        std::cerr << output << std::endl;
-        std::exit(0);
-    };
-}
-
-auto Find(bool& exists) -> std::function<void()>
-{
-    exists = false;
-    return [&exists]() { exists = true; };
-}
-
-auto Get(std::string& str) -> std::function<void(std::string)>
-{
-    return [&str](std::string value) { str = value; };
-}
-
-auto Get(int& i) -> std::function<void(std::string)>
-{
-    return [&i](std::string value) { i = std::stoi(value); };
-}
-
-auto Get(double& d) -> std::function<void(std::string)>
-{
-    return [&d](std::string value) { d = std::stod(value); };
-}
-
 class Parser
 {
     auto MakeOptionList() const -> std::string;
@@ -100,6 +69,37 @@ auto Parser::MakeOptionList() const -> std::string
     option_list << '\n';
 
     return option_list.str();
+}
+
+auto Usage(const std::string& output) -> std::string { return output; }
+
+auto Print(const std::string& output) -> std::function<void()>
+{
+    return [output]() {
+        std::cerr << output << std::endl;
+        std::exit(0);
+    };
+}
+
+auto Find(bool& exists) -> std::function<void()>
+{
+    exists = false;
+    return [&exists]() { exists = true; };
+}
+
+auto Get(std::string& str) -> std::function<void(std::string)>
+{
+    return [&str](std::string value) { str = value; };
+}
+
+auto Get(int& i) -> std::function<void(std::string)>
+{
+    return [&i](std::string value) { i = std::stoi(value); };
+}
+
+auto Get(double& d) -> std::function<void(std::string)>
+{
+    return [&d](std::string value) { d = std::stod(value); };
 }
 
 } // namespace opts
