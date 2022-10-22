303da3bca877a7523fe392a13c869dfdbc2290c7
Chris Thrasher <chrisjthrasher@gmail.com>
2022-10-21 20:59:00 -0600

    Delegate all documentation to documentation website

--- docs/Mainpage.hpp
+++ docs/Mainpage.hpp
@@ -37,10 +37,9 @@
 ///
 /// Create an instance of `argon::Parser`. Use `add_option` to add as many options as you want. The first argument is a
 /// comma-delimited string of flags and aliases. The second is the decription which appears in the help output. The
-/// third argument is either a string constant which gets printed to the console or its a mutable references that gets
-/// modified according to what the parser detects. If a string constant is a provided, a fourth argument is an enum
-/// where `argon::USAGE` indicates that a usage text is printed after the provided string whereas `argon::PRINT` simply
-/// prints the provided string.
+/// third argument is a mutable references that gets modified according to what the parser detects. An additional
+/// 4-argument overload is provided for printing messages to the console where `argon::USAGE` indicates that a usage
+/// text is printed after the provided string whereas `argon::PRINT` simply prints the provided string then exits.
 ///
 /// Here's what the formatted usage information looks like:
 ///
