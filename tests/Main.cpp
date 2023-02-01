a373cb696fd2f6411125b4222d8b11a00c6c064a
Chris Thrasher <chrisjthrasher@gmail.com>
2023-01-31 23:23:34 -0700

    Use test executable to also test flags

--- tests/Main.cpp
+++ tests/Main.cpp
@@ -1,16 +1,16 @@
 #include <argon/Parser.hpp>
 
-#include <iostream>
+#include <catch2/catch_session.hpp>
 
 int main(int argc, char* argv[])
-try {
+{
     bool debug = false;
     int count = 0;
     double speed = 0.0;
     std::string output;
 
     argon::Parser parser(argc, argv);
-    parser.add_option("h,help", "Show this help text", argon::USAGE, "Argon CLI parser example program");
+    parser.add_option("h,help", "Show this help text", argon::USAGE, "Argon CLI parser test program");
     parser.add_option("version", "Print program version", argon::PRINT, "v0.0.0");
     parser.add_option("d,debug", "Debug output", debug);
     parser.add_option("c,count", "Number of instances", count);
@@ -19,9 +19,5 @@ try {
     parser.add_position("filename", "Input file");
     parser.parse();
 
-    const auto filename = parser.get_position(0);
-    std::cout << "Filename: " << filename << '\n';
-} catch (const std::exception& ex) {
-    std::cerr << ex.what() << '\n';
-    return -1;
+    return Catch::Session().run(argc, argv);
 }
