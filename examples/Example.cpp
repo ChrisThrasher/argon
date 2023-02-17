a5540246ef7d0a31e7728769f0139671005ec34d
Chris Thrasher <chrisjthrasher@gmail.com>
2023-02-16 23:29:15 -0700

    Use examples to test install interface

---
+++ examples/Example.cpp
@@ -0,0 +1,27 @@
+#include <argon/Parser.hpp>
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+try {
+    bool debug = false;
+    int count = 0;
+    double speed = 0.0;
+    std::string output;
+
+    argon::Parser parser(argc, argv);
+    parser.add_option("h,help", "Show this help text", argon::USAGE, "Argon CLI parser test program");
+    parser.add_option("version", "Print program version", argon::PRINT, "v0.0.0");
+    parser.add_option("d,debug", "Debug output", debug);
+    parser.add_option("c,count", "Number of instances", count);
+    parser.add_option("speed", "Initial speed", speed);
+    parser.add_option("o,output", "Output filename", output);
+    parser.add_position("filename", "Input file");
+    parser.parse();
+
+    const auto filename = parser.get_position(0);
+    std::cout << "Filename: " << filename << '\n';
+} catch (const std::exception& ex) {
+    std::cerr << ex.what() << '\n';
+    return -1;
+}
