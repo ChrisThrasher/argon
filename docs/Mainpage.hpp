30d1328e232c71f63b2f14571dfab53f37c37951
Chris Thrasher <chrisjthrasher@gmail.com>
2023-02-16 23:24:04 -0700

    Fix example in docs

--- docs/Mainpage.hpp
+++ docs/Mainpage.hpp
@@ -39,14 +39,15 @@
 ///     argon::Parser parser(argc, argv);
 ///     parser.add_option("h,help", "Show this help text", argon::USAGE, "Argon CLI parser example program");
 ///     parser.add_option("version", "Print program version", argon::PRINT, "v0.0.0");
-///     parser.add_option(debug, "d,debug", "Debug output");
-///     parser.add_option(count, "c,count", "Number of instances");
-///     parser.add_option(speed, "speed", "Initial speed");
-///     parser.add_option(output, "o,output", "Output filename");
+///     parser.add_option("d,debug", "Debug output", debug);
+///     parser.add_option("c,count", "Number of instances", count);
+///     parser.add_option("speed", "Initial speed", speed);
+///     parser.add_option("o,output", "Output filename", output);
 ///     parser.add_position("filename", "Input file");
 ///     parser.parse();
 ///
 ///     const auto filename = parser.get_position(0);
+///     std::cout << "Filename: " << filename << '\n';
 /// } catch (const std::exception& ex) {
 ///     std::cerr << ex.what() << '\n';
 ///     return -1;
