9a2982c5471f06ca1d97f8d2d8a1aab3055ba456
Chris Thrasher <chrisjthrasher@gmail.com>
2023-06-06 12:51:05 -0600

    Reenable clang-tidy check

--- include/argon/Argument.hpp
+++ include/argon/Argument.hpp
@@ -14,7 +14,7 @@ class ARGON_EXPORT Argument {
 protected:
     static inline constexpr int m_format_width { 24 };
 
-    std::string m_description;
+    std::string m_description; // NOLINT(cppcoreguidelines-non-private-member-variables-in-classes)
 
 public:
     Argument(std::string description);
