fa7b4b75d49eefb1cbeb7bc236dbdcc14e80e407
Chris Thrasher <chrisjthrasher@gmail.com>
2023-09-15 16:23:09 -0600

    Shallow clone Catch2

--- tests/FindCatch2.cmake
+++ tests/FindCatch2.cmake
@@ -2,7 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(Catch2
     GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-    GIT_TAG v${Catch2_FIND_VERSION})
+    GIT_TAG v${Catch2_FIND_VERSION}
+    GIT_SHALLOW ON)
 FetchContent_MakeAvailable(Catch2)
 set_target_properties(Catch2 PROPERTIES COMPILE_OPTIONS "" EXPORT_COMPILE_COMMANDS OFF)
 set_target_properties(Catch2WithMain PROPERTIES EXPORT_COMPILE_COMMANDS OFF)
